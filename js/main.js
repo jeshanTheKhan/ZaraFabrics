5523b2c1b07143aa7ca7f1479cb004f654e4a8ac
GranPeradisoos <granparadiso729@gmail.com>
2024-10-28 05:49:59 +0600

    ZaraFabrics

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function updateQuantity(button, change) {
+    const quantityElement = button.parentElement.querySelector('.quantity-number');
+    let currentQuantity = parseInt(quantityElement.textContent);
+
+    currentQuantity += change;
+    if (currentQuantity < 1) {
+        currentQuantity = 1; // Prevent quantity from going below 1
+    }
+    quantityElement.textContent = currentQuantity;
+}
+
+function deleteProduct(button) {
+    const productRow = button.parentElement.parentElement;
+    productRow.remove();
+}
